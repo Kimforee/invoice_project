2f5eab1a3b8718e0b18c1d733bc1b0ec124133a4
Kimforee <97099667+Kimforee@users.noreply.github.com>
2024-03-22 12:07:34 +0530

    Added CRUD functionality
    updation and deletion where added

--- inv_app/tests.py
+++ inv_app/tests.py
@@ -22,21 +22,46 @@ class InvoiceAPITestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['customer_name'], 'Test Customer')
 
+    def test_update_invoice(self):
+        data = {
+            'date': '2024-03-20',  # Updated date
+            'customer_name': 'Updated Customer Name'  # Updated customer name
+        }
+        url = reverse('invoice-detail', args=[self.invoice.pk])
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['date'], '2024-03-20')
+        self.assertEqual(response.data['customer_name'], 'Updated Customer Name')
+
+    def test_delete_invoice(self):
+        url = reverse('invoice-detail', args=[self.invoice.pk])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Invoice.objects.filter(pk=self.invoice.pk).exists())
 
 class InvoiceDetailAPITestCase(APITestCase):
     def setUp(self):
         self.invoice = Invoice.objects.create(date='2024-03-19', customer_name='Test Customer')
         self.invoice_detail = InvoiceDetail.objects.create(invoice=self.invoice, description='Test Description', quantity=2, unit_price=10, price=20)
 
-    def test_create_invoice_detail(self):
+    def test_update_invoice_detail(self):
         data = {
             'invoice': self.invoice.pk,
-            'description': 'Test Description',
-            'quantity': 2,
-            'unit_price': 10,
-            'price': 20
+            'description': 'Updated Description',
+            'quantity': 3,
+            'unit_price': 15,
+            'price': 45
         }
-        url = reverse('invoicedetail-list')
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(InvoiceDetail.objects.count(), 2) 
+        url = reverse('invoicedetail-detail', args=[self.invoice_detail.pk])
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['description'], 'Updated Description')
+        self.assertEqual(response.data['quantity'], 3)
+        self.assertEqual(int(float(response.data['unit_price'])), 15)
+        self.assertEqual(int(float(response.data['price'])), 45)
+
+    def test_delete_invoice_detail(self):
+        url = reverse('invoicedetail-detail', args=[self.invoice_detail.pk])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(InvoiceDetail.objects.filter(pk=self.invoice_detail.pk).exists())
--- inv_app/views.py
+++ inv_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
 from .models import Invoice, InvoiceDetail
 from .serializers import InvoiceSerializer, InvoiceDetailSerializer
@@ -7,8 +8,31 @@ from .serializers import InvoiceSerializer, InvoiceDetailSerializer
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
+    def update(self, request, pk=None):
+        invoice = self.get_object()
+        serializer = self.get_serializer(invoice, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        invoice = self.get_object()
+        invoice.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class InvoiceDetailViewSet(viewsets.ModelViewSet):
     queryset = InvoiceDetail.objects.all()
     serializer_class = InvoiceDetailSerializer
+
+    def update(self, request, pk=None):
+        invoice_detail = self.get_object()
+        serializer = self.get_serializer(invoice_detail, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        invoice_detail = self.get_object()
+        invoice_detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
